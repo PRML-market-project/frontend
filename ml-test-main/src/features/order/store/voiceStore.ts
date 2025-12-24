4f1d12995f6a2cc9597e560d234ded4c89023481
DONGHYUK29 <brant1077@naver.com>
2025-12-24 16:05:26 +0900

    fix: vercel build type errors ver.2

--- ml-test-main/src/features/order/store/voiceStore.ts
+++ ml-test-main/src/features/order/store/voiceStore.ts
@@ -3,9 +3,22 @@ import { create } from 'zustand';
 interface VoiceStore {
   isCovered: boolean;
   setIsCovered: (isCovered: boolean) => void;
+
+  isMicOn: boolean;
+  startMic: () => void;
+  stopMic: () => void;
+  startHotwordDetection: () => void;
+  stopHotwordDetection: () => void;
 }
 
 export const useVoiceStore = create<VoiceStore>((set) => ({
   isCovered: true,
   setIsCovered: (isCovered) => set({ isCovered }),
+
+  isMicOn: false,
+  startMic: () => set({ isMicOn: true }),
+  stopMic: () => set({ isMicOn: false }),
+
+  startHotwordDetection: () => {},
+  stopHotwordDetection: () => {},
 }));
