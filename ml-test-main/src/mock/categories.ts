6538a9e5e31609fa572bc10e7ca044ad04f90fd3
DONGHYUK29 <brant1077@naver.com>
2026-01-20 18:15:59 +0900

    mock 데이터 오류

--- ml-test-main/src/mock/categories.ts
+++ ml-test-main/src/mock/categories.ts
@@ -6,11 +6,13 @@ export const mockCategories = [
   {
     category_id: 1,
     category_name: '음료',
-    category_name_en: 'beverage'
+    category_name_en: 'beverage',
+    category_type: '음료'
   },
   {
     category_id: 2,
     category_name: '주류',
-    category_name_en: 'drink'
+    category_name_en: 'drink',
+    category_type: '주류'
   },
 ];
